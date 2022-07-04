a3316410b9c1bce05e368d1dc64356991504776c
Vit <vit_waladzko@mail.ru>
2022-07-04 23:51:16 +0300

    04.07.2022

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,12 +1,18 @@
 import React from 'react'
+import {PATH} from "./RoutesPages";
+import {NavLink} from 'react-router-dom';
+import s from './Header.module.css'
 
 function Header() {
-    return (
-        <div>
-            // add NavLinks
 
-        </div>
-    )
+    return (
+        <nav className={s.header}>
+            <NavLink className={s.link} to={PATH.PRE_JUNIOR}>Pre-junior</NavLink>
+            <NavLink className={s.link} to={PATH.JUNIOR}>Junior</NavLink>
+            <NavLink className={s.link}  to={PATH.JUNIOR_PLUS}>Junior+</NavLink>
+            <div className={s.block}>Menu</div>
+        </nav>
+    );
 }
 
 export default Header
