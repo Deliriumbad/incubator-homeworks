0cc0b9bad157ade17766b58ff40129a13883d611
Vit <vit_waladzko@mail.ru>
2022-07-05 11:39:21 +0300

    05.07.2022

--- src/p1-main/m1-ui/u1-app/App.tsx
+++ src/p1-main/m1-ui/u1-app/App.tsx
@@ -5,14 +5,8 @@ import HW5 from "../../../p2-homeworks/h5/HW5";
 function App() {
     return (
         <div className={s.App}>
-
             <h2>React homeworks:</h2>
-            {/*<HW1/>
-            <HW2/>
-            <HW3/>
-            <HW4/>*/}
             <HW5/>
-
         </div>
     )
 }
--- src/p2-homeworks/h7/HW7.tsx
+++ src/p2-homeworks/h7/HW7.tsx
@@ -1,18 +1,17 @@
-import React, {useState} from 'react'
-import SuperSelect from './common/c5-SuperSelect/SuperSelect'
-import SuperRadio from './common/c6-SuperRadio/SuperRadio'
+import React, {useState} from 'react';
+import SuperSelect from './common/c5-SuperSelect/SuperSelect';
+import SuperRadio from './common/c6-SuperRadio/SuperRadio';
+import s from './HW7.module.css';
 
-const arr = ['x', 'y', 'z']
+const arr = ['HTML', 'JS', 'CSS']
 
 function HW7() {
+
     const [value, onChangeOption] = useState(arr[1])
 
     return (
-        <div>
-            <hr/>
-            homeworks 7
-
-            {/*should work (должно работать)*/}
+        <div className={s.container}>
+            <h3>homeworks 7</h3>
             <div>
                 <SuperSelect
                     options={arr}
@@ -28,12 +27,9 @@ function HW7() {
                     onChangeOption={onChangeOption}
                 />
             </div>
-
-            <hr/>
             {/*для личного творчества, могу проверить*/}
             {/*<AlternativeSuperSelect/>*/}
             {/*<AlternativeSuperRadio/>*/}
-            <hr/>
         </div>
     )
 }
