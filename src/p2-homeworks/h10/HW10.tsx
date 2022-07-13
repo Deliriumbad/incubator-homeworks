af4459f8e9f5f3339fb2281e9de5b6d4ad1666c0
Vit <vit_waladzko@mail.ru>
2022-07-13 17:16:42 +0300

    13.07.2022

--- src/p2-homeworks/h10/HW10.tsx
+++ src/p2-homeworks/h10/HW10.tsx
@@ -1,36 +1,36 @@
-import React from 'react'
-import {SuperButton} from '../h4/common/c2-SuperButton/SuperButton'
+import React from 'react';
+import {SuperButton} from '../h4/common/c2-SuperButton/SuperButton';
+import {useDispatch, useSelector} from "react-redux";
+import {AppStoreType} from "./bll/store";
+import {loadingAC} from "./bll/loadingReducer";
+import s from './HW10.module.css';
 
 function HW10() {
-    // useSelector, useDispatch
-    const loading = false
+    const isLoading = useSelector<AppStoreType, boolean>(state => state.loading.isLoading);
+    const dispatch = useDispatch();
 
     const setLoading = () => {
-        // dispatch
-        // setTimeout
-        console.log('loading...')
+        dispatch(loadingAC(true));
+
+        setTimeout(()=>{
+            dispatch(loadingAC(false));
+        }, 2000);
     };
 
     return (
-        <div>
-            <hr/>
-            homeworks 10
-
-            {/*should work (должно работать)*/}
-            {loading
+        <div className={s.container}>
+            <h3>homeworks 10</h3>
+            {isLoading
                 ? (
-                    <div>крутилка...</div>
+                    <div style={{height: '62px'}}>
+                        <img src={'https://cdn-icons-png.flaticon.com/512/154/154376.png'} />
+                    </div>
                 ) : (
                     <div>
-                        <SuperButton onClick={setLoading}>set loading...</SuperButton>
+                        <SuperButton onClick={setLoading}>Set loading</SuperButton>
                     </div>
                 )
             }
-
-            <hr/>
-            {/*для личного творчества, могу проверить*/}
-            {/*<Alternative/>*/}
-            <hr/>
         </div>
     )
 }
